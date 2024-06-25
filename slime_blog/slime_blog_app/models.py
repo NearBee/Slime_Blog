ff55c96db8d619a620427eadbfde8a72c0ed0cab
Nearbee <rexar14@gmail.com>
2024-06-25 12:01:39 -0500

    Created/updated creating a post

--- slime_blog/slime_blog_app/models.py
+++ slime_blog/slime_blog_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField() # CKEditor 5 should enhance this field
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- slime_blog/slime_blog_app/views.py
+++ slime_blog/slime_blog_app/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Post, Comment
+from .forms import PostForm
 
 # Create your views here.
 
+
 def homepage_view(request):
-    return render(request, 'homepage.html')
+    posts = Post.objects.all()
+    return render(request, "homepage.html", {"posts": posts})
+
+
+def post_details_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post=post)
+    return render(request, "post_details.html", {"post": post, "comments": comments})
+
+
+# @login_required
+def create_post(request):
+    if request.method == "POST":
+        # Handle form submission here
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("homepage")
+
+        else:
+            # TODO: Expand on form errors here
+            return render(request, "blog/create_post.html", {"form": form})
+    else:
+        form = PostForm()
+        return render(request, "create_post.html", {"form": form})
