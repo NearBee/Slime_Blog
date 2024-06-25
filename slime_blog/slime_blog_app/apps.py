aa1e5e7ed696720d9ca6f8fee264d81ea656f6af
Nearbee <rexar14@gmail.com>
2024-06-25 09:18:29 -0500

    Beginning of Project "Slime Blog"

---
+++ slime_blog/slime_blog_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SlimeBlogAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'slime_blog_app'
---
+++ slime_blog/slime_blog_app/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.post.title} at {self.created_at}'
