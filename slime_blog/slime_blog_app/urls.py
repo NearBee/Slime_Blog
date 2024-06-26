a6f463857182f60b791b3b3a7c7cd75d20f2e5a7
Nearbee <rexar14@gmail.com>
2024-06-26 15:27:21 -0500

    Change href for brand image to redirect to homepage

--- slime_blog/slime_blog_app/urls.py
+++ slime_blog/slime_blog_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_view, name='homepage'),
-    path('post/<int:pk>/', views.post_details_view, name='post_details'),
-    path('post/new/', views.create_post, name='create_post'),
+    path("home/", views.homepage_view, name="homepage"),
+    path("post/<int:pk>/", views.post_details_view, name="post_details"),
+    path("post/new/", views.create_post, name="create_post"),
 ]
