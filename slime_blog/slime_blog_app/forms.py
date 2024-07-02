b169c472622f6dff60022c075b4d976984f53ea4
Nearbee <rexar14@gmail.com>
2024-07-02 18:35:04 -0500

    Created a sign up form

--- slime_blog/slime_blog_app/forms.py
+++ slime_blog/slime_blog_app/forms.py
@@ -1,10 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 from .models import Post
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        help_text="Required. Enter a valid email address.",
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ["title", "content"]
         widgets = {
-            'content': forms.Textarea(attrs={'id': 'content'}),
+            "content": forms.Textarea(attrs={"id": "content"}),
         }
