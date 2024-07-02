e19d3bff31af24079720e50a45ded4e150141778
Nearbee <rexar14@gmail.com>
2024-07-02 18:35:53 -0500

    Added register/login/logout to path

--- slime_blog/slime_blog_app/urls.py
+++ slime_blog/slime_blog_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 from . import views
 
@@ -7,4 +8,12 @@ urlpatterns = [
     path("home/", views.homepage_view, name="homepage"),
     path("post/<int:pk>/", views.post_details_view, name="post_details"),
     path("post/new/", views.create_post, name="create_post"),
+    # TODO: Add /account/ to each when account page is created
+    path("register/", views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="homepage"), name="logout"),
 ]
