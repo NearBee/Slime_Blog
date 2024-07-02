38baebf50a0a7a0cf8a2762f22f0936128a3448e
Nearbee <rexar14@gmail.com>
2024-07-02 14:16:19 -0500

    Updated urls by adding redirectview so empty address bar now redirects to /home/

--- slime_blog/slime_blog_app/urls.py
+++ slime_blog/slime_blog_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="home/", permanent=False)),
     path("home/", views.homepage_view, name="homepage"),
     path("post/<int:pk>/", views.post_details_view, name="post_details"),
     path("post/new/", views.create_post, name="create_post"),
